e09ada3f0f890d51d3fe539948f74e34a594b257
unknown <papisettynagendra>
2022-08-15 09:36:31 +0530

    added : stock account management

---
+++ StockAccountManagement.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.demo;
+
+public class StockAccountManagement {
+    public void addStocks(Stock newShare) {
+
+    }
+
+    public void getStockDetails() {
+
+    }
+
+    public void calculateShareValue() {
+
+    }
+
+    public void removeStock(String shareName) {
+
+    }
+}
